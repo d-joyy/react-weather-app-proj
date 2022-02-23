4fa59e6e1a1c4301be4cc855bf1cde1245ece8df
Danielle <daniellejoyy91@gmail.com>
2022-02-22 23:11:13 -0500

    Added the GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Danielle and is{" "}
+        <a
+          href="https://github.com/d-joyy/react-weather-app-proj"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
